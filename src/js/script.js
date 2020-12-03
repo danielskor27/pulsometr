c07e34d8fee6b6bc4d0add7b73e53e1e29dacc11
daniel <daniels.kor@gmail.com>
2020-12-03 17:35:08 +0200

    catalog_prep

--- src/js/script.js
+++ src/js/script.js
@@ -4,15 +4,15 @@ $(document).ready(function(){
         adaptivesHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/chevron-left-solid.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../icons/chevron-right-solid.png"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    dots: true,
-                    arrows: false
-                }
-            }
+        // responsive: [
+        //     {
+        //         breakpoint: 992,
+        //         settings: {
+        //             dots: true,
+        //             arrows: false
+        //         }
+        //     }
 
-        ]
+        // ]
     });
   }); 
